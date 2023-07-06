efa6a3895b05d78e25cdbc33f49974b50913f9ed
perfumegoddess <queenjossey@gmail.com>
2023-07-06 15:24:04 +0000

    jgu

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,42 @@
-def canUnlockAll(boxes):
-    num_boxes = len(boxes)
-    unlocked = [False] * num_boxes
-    unlocked[0] = True  # The first box is always unlocked
+#!/usr/bin/python3
+"""A python module that determines if all boxes can be opened
+   from a list of lists
+"""
 
-    for box in range(num_boxes):
-        if unlocked[box]:
-            for key in boxes[box]:
-                if key < num_boxes and not unlocked[key]:
-                    unlocked[key] = True
 
-    return all(unlocked)
+def canUnlockAll(boxes=[]):
+    """A function that returns True of all box in
+    boxes can be opend
+    """
+    if not boxes:
+        return False
 
+    keys = set([0])
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
+
+    if len(keys) != len(boxes):
+        return False
+
+    return True
+
+
+if __name__ == '__main__':
+    boxes = [
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(unlockBoxes(boxes))
